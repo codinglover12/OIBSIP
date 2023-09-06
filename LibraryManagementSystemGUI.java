96713aa0ffd90a305868b2e45fa941ea9f9b9c4b
codinglover12 <135342331+codinglover12@users.noreply.github.com>
2023-09-06 15:30:44 +0530

    Add files via upload
    Library_ManagementSystem_Original

--- LibraryManagementSystemGUI.java
+++ LibraryManagementSystemGUI.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,17 +13,19 @@ import java.sql.SQLException;
 
 public class LibraryManagementSystemGUI {
 
-    private static final String DEFAULT_USERNAME = "admin";
+    private static final String DEFAULT_USERNAME = "Atul Kumar";
     private static final String DEFAULT_PASSWORD = "Tech123";
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/librarymanagement";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "Techmaster@123";
 
     private Connection connection;
+    private String enteredUsername;
+    private String enteredPassword;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            new LibraryManagementSystemGUI().createAndShowGUI();
+            new LibraryManagementSystemGUI().showLoginDialog();
         });
     }
 
@@ -34,52 +37,154 @@ public class LibraryManagementSystemGUI {
         }
     }
 
+    private void showLoginDialog() {
+        JFrame loginFrame = new JFrame("Login");
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.setSize(400, 200); // Increased width and height
+        loginFrame.setResizable(false);
+
+        JPanel loginPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(10, 10, 10, 10); // Increased padding between components
+
+        JLabel usernameLabel = new JLabel("Username:");
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        loginPanel.add(usernameLabel, constraints);
+
+        JTextField usernameField = new JTextField(20);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        loginPanel.add(usernameField, constraints);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        loginPanel.add(passwordLabel, constraints);
+
+        JPasswordField passwordField = new JPasswordField(20);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        loginPanel.add(passwordField, constraints);
+
+        // Add icons to the labels (Replace with your icon URLs)
+        try {
+            URL userIconUrl = new URL("https://icon-library.com/images/status-icon-png/status-icon-png-24.jpg");
+            URL passwordIconUrl = new URL("https://cdn-icons-png.flaticon.com/512/564/564633.png");
+
+            ImageIcon userIcon = new ImageIcon(userIconUrl);
+            ImageIcon passwordIcon = new ImageIcon(passwordIconUrl);
+
+            // Scale down the icons for visibility
+            Image userImage = userIcon.getImage().getScaledInstance(30, 32, Image.SCALE_SMOOTH);
+            Image passwordImage = passwordIcon.getImage().getScaledInstance(30, 32, Image.SCALE_SMOOTH);
+
+            usernameLabel.setIcon(new ImageIcon(userImage));
+            passwordLabel.setIcon(new ImageIcon(passwordImage));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JButton loginButton = new JButton("Login");
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2; // Span two columns
+        constraints.anchor = GridBagConstraints.CENTER; // Center-align the button
+        loginPanel.add(loginButton, constraints);
+
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                enteredUsername = usernameField.getText();
+                enteredPassword = new String(passwordField.getPassword());
+
+                if (isValidLogin(enteredUsername, enteredPassword)) {
+                    loginFrame.dispose(); // Close the login dialog
+                    createAndShowGUI(); // Show the main GUI
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid login credentials.", "Login Failed", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        loginFrame.getContentPane().add(loginPanel);
+        loginFrame.setVisible(true);
+        loginFrame.setLocationRelativeTo(null); // Center the login dialog on the screen
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private boolean isValidLogin(String username, String password) {
+        // You can customize this method to check credentials from your database.
+        // For now, it compares with the default credentials.
+        return username.equals(DEFAULT_USERNAME) && password.equals(DEFAULT_PASSWORD);
+    }
+
     private void createAndShowGUI() {
         JFrame frame = new JFrame("Library Management System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
         JPanel panel = new JPanel(new GridLayout(5, 1));
-        JButton addBookButton = new JButton("Add Book");
-        JButton issueBookButton = new JButton("Issue Book");
-        JButton returnBookButton = new JButton("Return Book");
-        JButton printDetailsButton = new JButton("Print Details");
-        JButton exitButton = new JButton("Exit");
 
+        Font buttonFont = new Font("Arial", Font.LAYOUT_NO_LIMIT_CONTEXT, 40);
+
+        JButton addBookButton = new JButton("Add Book");
+        addBookButton.setFont(buttonFont);
         addBookButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 addBook();
             }
         });
+        panel.add(addBookButton);
 
+        JButton issueBookButton = new JButton("Issue Book");
+        issueBookButton.setFont(buttonFont);
         issueBookButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 issueBook();
             }
         });
+        panel.add(issueBookButton);
 
+        JButton returnBookButton = new JButton("Return Book");
+        returnBookButton.setFont(buttonFont);
         returnBookButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 returnBook();
             }
         });
+        panel.add(returnBookButton);
 
+        JButton printDetailsButton = new JButton("Print Details");
+        printDetailsButton.setFont(buttonFont);
         printDetailsButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 printDetails();
             }
         });
+        panel.add(printDetailsButton);
 
+        JButton exitButton = new JButton("Exit");
+        exitButton.setFont(buttonFont);
         exitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 exit();
             }
         });
-
-        panel.add(addBookButton);
-        panel.add(issueBookButton);
-        panel.add(returnBookButton);
-        panel.add(printDetailsButton);
         panel.add(exitButton);
 
         frame.getContentPane().add(panel);
